397b07b91e08c217a48f8746fb5ed9ecdda847ef
Trương Luân <naul.truong@siliconstack.com.au>
2023-11-14 19:50:59 +0700

    Create Api Get result Test of User

---
+++ API/Quiz.DTO/UserTestManagement/GetUserTestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.DTO.UserTestManagement
+{
+    public class GetUserTestResponse
+    {
+        public string UserTestId { get; set; }
+        public string UserId { get; set; }
+        public int CorrectAnswers { get; set; }
+        public decimal Score { get; set; }
+        public string TestStructureId { get; set; }
+        public string TestStructureName{ get; set; }
+        public int NumberOfQuestions { get; set; }
+        public int Time { get; set; }
+    }
+}
